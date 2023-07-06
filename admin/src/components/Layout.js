1a5696d2caa5e905d1c55369e67a71a160cbc7f7
Harwin14 <agungharwinn@gmail.com>
2023-07-06 19:25:47 +0700

    mobile responsive

--- admin/src/components/Layout.js
+++ admin/src/components/Layout.js
@@ -2,10 +2,13 @@ import Nav from "@/components/Nav";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 import { Poppins } from "next/font/google";
+import { useState } from "react";
+import Logo from "./Logo";
 
 const font = Poppins({ weight: "400", subsets: ["latin"] });
 
 export default function Layout({ children }) {
+    const [showNav, setShowNav] = useState(false);
     const { data: session } = useSession();
     if (!session) {
         return (
@@ -24,11 +27,32 @@ export default function Layout({ children }) {
         );
     }
     return (
-        <div className="bg-gray-200 min-h-screen  flex">
-            <Nav />
-            <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
-                {children}
-               
+        <div className="bg-gray-200 min-h-screen ">
+            <div className="block md:hidden flex items-center">
+                <button onClick={() => setShowNav(true)}>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-6 h-6"
+                    >
+                        <path
+                            fillRule="evenodd"
+                            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </button>
+                <div className="flex grow justify-center mr-6">
+
+                <Logo/>
+                </div>
+            </div>
+            <div className="flex">
+                <Nav show={showNav} />
+                <div className="bg-white flex-grow mt-2 mr-2 mb-2 rounded-lg p-4">
+                    {children}
+                </div>
             </div>
         </div>
     );

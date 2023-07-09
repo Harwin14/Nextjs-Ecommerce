3534b07553bd8d6c83c113e17db7cdd6604d60e5
Harwin14 <agungharwinn@gmail.com>
2023-07-09 10:25:10 +0700

    r

--- admin/src/components/Layout.js
+++ admin/src/components/Layout.js
@@ -60,7 +60,7 @@ export default function Layout({ children }) {
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
-                        viewBox="0 0 24 24"
+                        viewBox="0 0 24 24" 
                         stroke-width="1.5"
                         stroke="currentColor"
                         class="w-6 h-6"

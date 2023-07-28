3db98907b306643b2bf212307a592d3d598bf0e5
Harwin14 <agungharwinn@gmail.com>
2023-07-28 13:41:29 +0700

    r

--- admin/src/components/Layout.js
+++ admin/src/components/Layout.js
@@ -33,25 +33,6 @@ export default function Layout({ children }) {
     }
     return (
         <div className="bg-abu min-h-screen h-screen overflow-hidden relative">
-            {/* <div className="block md:hidden items-center p-4">
-                <button onClick={() => setShowNav(true)}>
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            fillRule="evenodd"
-                            d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
-                            clipRule="evenodd"
-                        />
-                    </svg>
-                </button>
-                <div className="flex grow justify-center mr-6">
-                <Logo/>
-                </div>
-            </div> */}
             {showNav ? (
                 <button
                     onClick={togleButton}
